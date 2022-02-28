65f1328e9ba485eb026052b72fe9aa834abb6646
Édouard <edouard@gomez-vaez.com>
2022-02-28 16:23:37 +0100

    Update production.js

--- production.js
+++ production.js
@@ -4,16 +4,24 @@ function computeFB(n) {
 }
 
 function printResult() {
-    let n = parseInt(document.getElementById("n").value);
+    let n = parseInt(document.getElementById("n").value)
     for (i=0; i<n; i++) {
-        let div = document.createElement("div");
-        div.textContent = computeFB(i);
-        document.getElementById("answer").appendChild(div);
+        let div = document.createElement("div")
+        div.textContent = computeFB(i)
+        document.getElementById("answer").appendChild(div)
     }
+}
+
+function greet() {
+    let div = document.createElement("div")
+    div.textContent = "Hello unit world!"
+    div.id = "greetings"
+    document.getElementById("answer").appendChild(div)
 }
 
 function loadBehavior() {
     document.getElementById("exec").addEventListener('click', () => printResult())
+    document.getElementById("greet").addEventListener('click', () => greet())
 }
 
 window.addEventListener('load', () => loadBehavior());
@@ -35,18 +43,41 @@ function testShouldRaiseAnError() {
     testCase.assertEquals(null, undefined.getFoo());
 }
 
+function testShouldBeAbleToHaveTwoAssertions() {
+    testCase.assertEquals(1, computeFB(1));
+    testCase.assertEquals(2, computeFB(2));
+}
+
+function testWhenIClickThenMessageShouldAppear() {
+    testCase.clickOnId("greet")
+    testCase.assertTextContentContains("greetings", "Hello unit world!")
+    testCase.assertTextContentContains("greetings", "Hello")
+    testCase.assertTextContentContains("greetings", "unit")
+    testCase.assertTextContentContains("greetings", "!")
+}
+
+
 /**
  * Unit test unit test (for kevin)
  */
 
 function testOneShouldReturnOneShouldWork() {
-	testCase.assertEquals("ok", document.getElementsByTagName("ul")[0].children[0].className)	 
+	testCase.assertEquals("ok", document.getElementById("testOneShouldReturnOne").className)	 
 }
 
 function testThreeShouldReturnFizzShouldFail() {
-	testCase.assertEquals("ko", document.getElementsByTagName("ul")[0].children[1].className)	 
+	testCase.assertEquals("ko", document.getElementById("testThreeShouldReturnFizz").className)	 
 }
 
 function testShouldRaiseAnErrorShouldRaiseAnError() {
-	testCase.assertEquals("error", document.getElementsByTagName("ul")[0].children[2].className)	 
+	testCase.assertEquals("error", document.getElementById("testShouldRaiseAnError").className)	 
+}
+
+function testShouldBeAbleToHaveTwoAssertionsShouldHaveTwoMessages() {
+	testCase.assertEquals("ok", document.getElementById("testShouldBeAbleToHaveTwoAssertions").className) 
+	testCase.assertEquals("ok", document.getElementById("testShouldBeAbleToHaveTwoAssertions-2").className) 
+}
+
+function testWhenIClickThenMessageShouldAppearShouldWork() {
+	testCase.assertEquals("ok", document.getElementById("testWhenIClickThenMessageShouldAppear").className)	 
 }
